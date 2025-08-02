7dba58df3bcfbf1de05864c6ccdb8795f6260d32
Clebson Moura <clebson.sh@gmail.com>
2025-08-02 13:24:50 -0300

    abstract method

---
+++ abstract_method.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'classes/Account.php';
+
+class SalaryAccount extends Account
+{
+    // Fatal error: Class SalaryAccount contains 1 abstract method
+    // and must therefore be declared abstract or implement the
+    // remaining methods (Account::withdraw)
+}
