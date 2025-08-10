87029352d3a63c830ca39273e923fae005ea7bb6
Clebson Moura <clebson.sh@gmail.com>
2025-08-10 15:23:12 -0300

    separate template for people list

--- level3/people_list.php
+++ level3/people_list.php
@@ -1,69 +1,28 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>List of people</title>
-        <link href="css/list.css" rel="stylesheet" type="text/css" media="screen">
-    </head>
-    <body>
-    <?php
+<?php
 
-    $dsn = 'host=localhost port=5432 dbname=book user=root password=password';
-    $conn = pg_connect($dsn);
+$dsn = 'host=localhost port=5432 dbname=book user=root password=password';
+$conn = pg_connect($dsn);
 
-    if (! empty($_GET['action']) and $_GET['action'] == 'delete') {
-        $id = (int) $_GET['id'];
-        pg_query($conn, "DELETE FROM people WHERE id='{$id}'");
-    }
+if (! empty($_GET['action']) and $_GET['action'] == 'delete') {
+    $id = (int) $_GET['id'];
+    pg_query($conn, "DELETE FROM people WHERE id='{$id}'");
+}
 
-    $result = pg_query($conn, 'SELECT * FROM people ORDER BY id');
+$result = pg_query($conn, 'SELECT * FROM people ORDER BY id');
 
-    echo '<table border=1>';
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th> </th>';
-    echo '<th> </th>';
-    echo '<th> Id </th>';
-    echo '<th> Name </th>';
-    echo '<th> Address </th>';
-    echo '<th> Neighborhood </th>';
-    echo '<th> Phone </th>';
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
+$items = '';
+while ($row = pg_fetch_assoc($result)) {
+    $item = file_get_contents('html/item.html');
+    $item = str_replace('{id}', $row['id'], $item);
+    $item = str_replace('{name}', $row['name'], $item);
+    $item = str_replace('{address}', $row['address'], $item);
+    $item = str_replace('{neighborhood}', $row['neighborhood'], $item);
+    $item = str_replace('{phone}', $row['phone'], $item);
+    $item = str_replace('{email}', $row['email'], $item);
+    $items .= $item;
+}
 
-    while ($row = pg_fetch_assoc($result)) {
-        $id = $row['id'];
-        $name = $row['name'];
-        $address = $row['address'];
-        $neighborhood = $row['neighborhood'];
-        $phone = $row['phone'];
-        $email = $row['email'];
-        $city_id = $row['city_id'];
-        echo '<tr>';
-        echo "<td align='center'>
-            <a href='person_form.php?action=edit&id={$id}'>
-                <img src='images/edit.svg' style='width:17px'>
-            </a>
-        </td>";
-        echo "<td align='center'>
-            <a href='people_list.php?action=delete&id={$id}'>
-                <img src='images/remove.svg' style='width:17px'>
-            </a>
-        </td>";
-        echo "<td> {$id} </td>";
-        echo "<td> {$name} </td>";
-        echo "<td> {$address} </td>";
-        echo "<td> {$neighborhood} </td>";
-        echo "<td> {$phone} </td>";
-        echo '</tr>';
-    }
+$list = file_get_contents('html/list.html');
+$list = str_replace('{items}', $items, $list);
 
-    echo '</tbody>';
-    echo '</table>';
-
-    ?>
-        <button onclick="window.location='person_form.php'">
-            <img src='images/insert.svg' style='width:17px'> Add
-        </button>
-    </body>
-</html>
+echo $list;
