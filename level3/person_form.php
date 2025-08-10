a1a8399006f18eb63d431e7cdb5fef6cd611449e
Clebson Moura <clebson.sh@gmail.com>
2025-08-10 14:53:51 -0300

    separate template for person form

--- level3/person_form.php
+++ level3/person_form.php
@@ -1,9 +1,5 @@
 <?php
 
-require_once 'city_combo_list.php';
-
-$id = $name = $address = $neighborhood = $phone = $email = $city_id = null;
-
 if (! empty($_REQUEST['action'])) {
     $dsn = 'host=localhost port=5432 dbname=book user=root password=password';
     $conn = pg_connect($dsn);
@@ -11,25 +7,10 @@ if (! empty($_REQUEST['action'])) {
     if ($_REQUEST['action'] == 'edit') {
         $id = (int) $_GET['id'];
         $result = pg_query($conn, "SELECT * FROM people WHERE id='{$id}'");
+        $person = pg_fetch_assoc($result);
 
-        if ($row = pg_fetch_assoc($result)) {
-            $id = $row['id'];
-            $name = $row['name'];
-            $address = $row['address'];
-            $neighborhood = $row['neighborhood'];
-            $phone = $row['phone'];
-            $email = $row['email'];
-            $city_id = $row['city_id'];
-        }
     } elseif ($_REQUEST['action'] == 'save') {
-        $id = $_POST['id'];
-        $name = $_POST['name'];
-        $address = $_POST['address'];
-        $neighborhood = $_POST['neighborhood'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-        $city_id = $_POST['city_id'];
-
+        $person = $_POST;
         if (empty($_POST['id'])) {
             $result = pg_query($conn, 'SELECT max(id) as next FROM people');
             $next = (int) pg_fetch_assoc($result)['next'] + 1;
@@ -48,26 +29,34 @@ if (! empty($_REQUEST['action'])) {
                 VALUES
                     (
                         '{$next}',
-                        '{$name}',
-                        '{$address}',
-                        '{$neighborhood}',
-                        '{$phone}',
-                        '{$email}',
-                        '{$city_id}'
+                        '{$person['name']}',
+                        '{$person['address']}',
+                        '{$person['neighborhood']}',
+                        '{$person['phone']}',
+                        '{$person['email']}',
+                        '{$person['city_id']}'
                     )
             SQL;
 
             $result = pg_query($conn, $query);
+
+            $person['id'] = null;
+            $person['name'] = '';
+            $person['address'] = '';
+            $person['neighborhood'] = '';
+            $person['phone'] = '';
+            $person['email'] = '';
+            $person['city_id'] = null;
         } else {
             $query = <<< SQL
                 UPDATE people
-                SET name = '{$name}',
-                    address = '{$address}',
-                    neighborhood = '{$neighborhood}',
-                    phone = '{$phone}',
-                    email = '{$email}',
-                    city_id = '{$city_id}'
-                WHERE id = '{$id}'
+                SET name = '{$person['name']}',
+                    address = '{$person['address']}',
+                    neighborhood = '{$person['neighborhood']}',
+                    phone = '{$person['phone']}',
+                    email = '{$person['email']}',
+                    city_id = '{$person['city_id']}'
+                WHERE id = '{$person['id']}'
             SQL;
 
             $result = pg_query($conn, $query);
@@ -76,40 +65,27 @@ if (! empty($_REQUEST['action'])) {
         echo $result ? 'Record saved successfully' : pg_last_error($conn);
         pg_close($conn);
 
-        $id = $name = $address = $neighborhood = $phone = $email = $city_id = null;
     }
+} else {
+    $person['id'] = null;
+    $person['name'] = '';
+    $person['address'] = '';
+    $person['neighborhood'] = '';
+    $person['phone'] = '';
+    $person['email'] = '';
+    $person['city_id'] = null;
 }
 
-?>
+require_once 'city_combo_list.php';
 
-<html>
+$form = file_get_contents('html/form.html');
+$form = str_replace('{id}', $person['id'], $form);
+$form = str_replace('{name}', $person['name'], $form);
+$form = str_replace('{address}', $person['address'], $form);
+$form = str_replace('{neighborhood}', $person['neighborhood'], $form);
+$form = str_replace('{phone}', $person['phone'], $form);
+$form = str_replace('{email}', $person['email'], $form);
+$form = str_replace('{city_id}', $person['city_id'], $form);
+$form = str_replace('{cities}', city_combo_list($person['city_id']), $form);
 
-<head>
-    <meta charset="uft-8">
-    <title>Person registration</title>
-    <link href="css/form.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
-
-<body>
-    <form enctype="multipart/form-data" method="post" action="person_form.php?action=save">
-        <label>ID</label>
-        <input name="id" readonly="1" type="text" style="width: 30%" value="<?= $id?>">
-        <label>Name</label>
-        <input name="name" type="text" style="width: 50%" value="<?= $name?>">
-        <label>Address</label>
-        <input name="address" type="text" style="width: 50%" value="<?= $address?>">
-        <label>Neighborhood</label>
-        <input name="neighborhood" type="text" style="width: 25%" value="<?= $neighborhood?>">
-        <label>Phone</label>
-        <input name="phone" type="text" style="width: 25%" value="<?= $phone?>">
-        <label>Email</label>
-        <input name="email" type="text" style="width: 25%" value="<?= $email?>">
-        <label>City</label>
-        <select name="city_id" style="width: 25%">
-        <?= city_combo_list($city_id) ?>
-        </select>
-        <input type="submit">
-    </form>
-</body>
-
-</html>
+echo $form;
